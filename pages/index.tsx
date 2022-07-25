9a84aeebead7e043f12e65027791e9e6d807e882
thevyansh <thevyansh@gmail.com>
2022-07-25 16:53:22 +0530

    Setup under development

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-// import {Logo} from '../components/Logo/index'
 
 const Home: NextPage = () => {
   return (
